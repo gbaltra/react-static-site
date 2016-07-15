26db475a9edd428798dc71da015e4c7e1a335c0e
Guillermo <gbaltrae@hotmail.com>
2016-07-15 14:00:48 -0400

    cleaning

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,10 +4,6 @@ var Header = require('./Header.jsx')
 
 var Root = React.createClass({
   render: function () {
-    var initialProps = {
-          __html: safeStringify(this.props)
-    };
-
     return (
       <html>
         <head>
@@ -23,22 +19,5 @@ var Root = React.createClass({
     )
   }
 })
-
-function safeStringify(obj) {
-  var cache = [];
-  var string = JSON.stringify(obj, function(key, value) {
-      if (typeof value === 'object' && value !== null) {
-          if (cache.indexOf(value) !== -1) {
-              // Circular reference found, discard key
-              return;
-            }
-            // Store value in our collection
-            cache.push(value);
-          }
-          return value;
-  });
-  cache = null; // Enable garbage collection
-  return string.replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--');
-}
 
 module.exports = Root
