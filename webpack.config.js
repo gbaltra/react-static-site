b7c98a329161e6533ddae4183770381d8fb928dd
Guillermo <gbaltrae@hotmail.com>
2016-07-15 23:20:40 -0400

    browser-sync

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var data = require('./src/data');
 
 module.exports = {
@@ -16,6 +18,17 @@ module.exports = {
   },
 
   plugins: [
-    new StaticSiteGeneratorPlugin('main', data.routes, data)
+    // reloads browser when the watched files change
+    new BrowserSyncPlugin({
+      // use existing Apache virtual host
+      proxy: 'http://localhost/',
+      //tunnel: true,
+      // watch the built files and the index file
+      files: ['dist/*']
+    }),
+    new StaticSiteGeneratorPlugin('main', data.routes, data),
+    new CopyWebpackPlugin([
+            { from: 'src/api', to: 'api' }
+    ])
   ]
 }
